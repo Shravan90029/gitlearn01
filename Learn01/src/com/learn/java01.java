6fae36ae6903fe68d91f50b6ece17432ee66040f
shravankalaskar <shravanece06@gmail.com>
2020-03-22 10:49:05 +0530

    add method

--- Learn01/src/com/learn/java01.java
+++ Learn01/src/com/learn/java01.java
@@ -2,6 +2,12 @@ package com.learn;
 
 public class java01 {
 	
+	public void add (){
+		int a, b,c;
+		 System.out.println("this is saddition");
+		 
+	}
+	
 	public static void main (String []args){
 		
 		System.out.println("this is a sample project");
