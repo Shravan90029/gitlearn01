ba493c455076c6ad1219864c41c3c4625d3a7e04
Shravan90029 <shravanece06@gmail.com>
2020-03-22 10:47:05 +0530

    Update java01.java

--- Learn01/src/com/learn/java01.java
+++ Learn01/src/com/learn/java01.java
@@ -3,8 +3,9 @@ package com.learn;
 public class java01 {
 	
 	public static void main (String []args){
-		
+		/// this is for sample comit erge scenerios
 		System.out.println("this is a sample project");
+		System.out.println("this is a sample project 02 line 2");
 	}
 
 }
